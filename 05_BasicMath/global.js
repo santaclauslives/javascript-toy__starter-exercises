2a680b2e1a99751bc22a48c834d474e4e27e306c
Patrick Clark <patclark1958@gmail.com>
2016-03-09 17:03:13 -0600

    Odd. Can't figure out why #5 not working. Not worried about the html page it is just stuff I carried over from #4. Not getting prompts.

---
+++ 05_BasicMath/global.js
@@ -0,0 +1,30 @@
+
+
+
+//ask for the user input 
+var firstNumber = prompt("Please enter the first number  : ");
+var secondNumber = prompt("Please enter the second number : ");
+var thirdNumber = prompt("Please enter the third number : ");
+
+//convert the prompt strings to integers
+firstNumber = parseInt(firstNumber);
+secondNumber = parseInt(secondNumber);
+thirdNumber = parseInt(thirdNumber);
+
+//do the math for my own clarification
+//added(firstNumber + secondNumber + thirdNumber);
+//subtracted(firstNumber - secondNumber - thirdNumber);
+//multiplied(firstNumber * secondNumber * thirdNumber);
+//divided(firstNumber / secondNumber);
+//incremented(firstNumber ++ );
+//decremented(secondNumber --);
+
+//output the results of the math as alerts
+//Yellow is strings that will show the operation, I think. Red is concatenation. The part in parentheses after the "=" does the actual math.
+alert("Added: " + firstNumber + " + " + secondNumber + " + " + thirdNumber + " = " + (firstNumber + secondNumber + thirdNumber));
+alert("Subtracted: " + firstNumber + " - " + secondNumber + " - " thirdNumber + " = " + (firstNumber - secondNumber - thirdNumber));
+alert("Multiplied: " + firstNumber + " * " + secondNumber + " * " + thirdNumber + " = " + (firstNumber * secondNumber * thirdNumber));
+alert("Divided: " + firstNumber + " / " + secondNumber + " = " + (firstNumber / secondNumber));
+alert("Incremented: " firstNumber + " ++ " + " = " (firstNumber++ ));
+alert("Decremented: " secondNumber + " -- " + " = " (secondNumber--));
+
