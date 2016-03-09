0d6b3d595c66ad9f9cdeec26037dea4d6a031c2e
Patrick Clark <patclark1958@gmail.com>
2016-03-09 13:46:06 -0600

    #3 questions were a little murky for me. I can do better.

---
+++ 02_OutputWithAlertAndLog/global.js
@@ -0,0 +1,14 @@
+//Looked on solution page for how to add this to html page. Also did not trust that I was doing this right but it looks like I was.
+
+//alert message to the user identifying me as Santa Claus
+alert("Hello. Or should I say hohoho. I am Santa Claus.");
+
+//information about me
+console.log("My favorite breakfast is to skip breakfast.");
+console.log("I have 2 cats.");
+console.log("My favorite place to eat lots of food is Modern Love, in part because my vegan wife has many choices there.");
+console.log("I take about 3 naps per week. None during Code School.");
+console.log("I would be happy with 7 naps per week.");
+
+//alert message that ends the dialogue - or more like monologue
+alert("Goodbye.");
---
+++ 03_Variables/global.js
@@ -0,0 +1,15 @@
+
+//03 Variables
+
+//assign values to three variables: name, age and hair color
+var name = "Santa Claus";
+var age = 432;
+var hairColor = "silver";
+
+//three alerts that use concatenation to include the variable info
+alert("My name is " + name);
+alert("I am " + age + " years old");
+alert("My hair color is " + hairColor);
+
+
+
