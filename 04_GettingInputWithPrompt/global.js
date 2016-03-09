9d033bf74507b057ee0c2179256cc24d67abc1a8
Patrick Clark <patclark1958@gmail.com>
2016-03-09 15:22:19 -0600

    #4 complete. I am now in the semicolon habit.

---
+++ 04_GettingInputWithPrompt/global.js
@@ -0,0 +1,24 @@
+
+//ask for the user input 
+var userName = prompt("Please enter your name : ")
+var userAge = prompt("Please enter your age : ")
+var userHometown = prompt("Please enter the name of your hometown : ")
+var userFaveNum = prompt("Please enter your favorite number : ")
+
+//return the user input as alerts
+alert("Your name is " + userName);
+alert("You are " + userAge + " years old");
+alert("You are from " + userHometown);
+alert("Your favorite number is " + userFaveNum);
+
+//return the user input as console.log
+console.log("Your name is " + userName);
+console.log("You are " + userAge + " years old");
+console.log("You are from " + userHometown);
+console.log("Your favorite number is " + userFaveNum);
+
+
+
+
+
+
