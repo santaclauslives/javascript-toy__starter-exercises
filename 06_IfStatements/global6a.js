8fd518227ae24fc2720dcd54d4552cdbfa6c1239
Patrick Clark <patclark1958@gmail.com>
2016-03-10 05:17:06 -0600

    starting point for Thursday. Done w 6b

---
+++ 06_IfStatements/global6a.js
@@ -0,0 +1,19 @@
+
+
+console.log( "5 == '5' is " + (5 == '5') );
+console.log( "5 == 'five' is " + (5 == 'five') );
+console.log( "5 == '6' is " + (5 == '6') );
+console.log( "5 === '5' is " + (5 === '5') );
+console.log( "5 == 2 + 3 is " + (5 == 2 + 3) );
+console.log( "5 == 5 is " + (5 == 5) );
+console.log( "5 === 5 is " + (5 === 5) );
+console.log( "true == 'true' is " + (true == 'true') );
+console.log( "true === 'true' is " + (true === 'true') );
+console.log( "true == false is " + (true == false) );
+console.log( "5 == 12 is " + (5 == 12) );
+console.log( "5 != 12 is " + (5 != 12) );
+console.log( "5 < 12 is " + (5 < 12) );
+console.log( "5 <= 12 is " + (5 <= 12) );
+console.log( "5 > 12 is " + (5 > 12));
+console.log( "5 >= 12 is " + (5 >= 12));
+
---
+++ 06_IfStatements/global6b.js
@@ -0,0 +1,14 @@
+
+
+var password = "8reindeer"
+var guess = prompt("Guess the password: ")
+
+if (guess === password)
+    {
+      alert("Well done!");
+    }
+  else
+    {
+      alert("Sorry, the password is " + password + " . ");
+    }
+
