c65f8ab6d44ad1239eb0996c4d52b2b23e2eeca7
Patrick Clark <patclark1958@gmail.com>
2016-03-10 07:46:53 -0600

    finished 6c

---
+++ 06_IfStatements/global6c.js
@@ -0,0 +1,14 @@
+
+
+var secretNumber = 57
+var guessNumber = prompt("Guess the secret number: ")
+
+if (secretNumber == guessNumber)
+    {
+      alert("Well done!");
+    }
+  else
+    {
+      alert("Sorry, the secret numbrer is " + secretNumber + " . ");
+    }
+
