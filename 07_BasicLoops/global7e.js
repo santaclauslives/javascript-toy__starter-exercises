daa4bb54ecaccf788be1f00a83369c1c4eefa3c0
Patrick Clark <patclark1958@gmail.com>
2016-03-11 15:06:15 -0600

    Quite possibly my favorite code I have ever written.

---
+++ 07_BasicLoops/global7e.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+for (index = 1; index <= 10; index+=1) {
+  console.log(index)
+}
+
+
+
+//Use a while loop to count from 1-10 (do your output either using `console.log()` or `alert()`).
