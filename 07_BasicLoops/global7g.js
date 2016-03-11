22ab3cad35771346ec540ff3f38d0d71e374ff4c
Patrick Clark <patclark1958@gmail.com>
2016-03-11 15:18:52 -0600

    7g done. Like the for loop.

---
+++ 07_BasicLoops/global7g.js
@@ -0,0 +1,11 @@
+//Truncated the song to get this done.
+
+//var startValue = 10; 
+//var song = ((startValue) + " bottles of beer on the wall. Take one down. Pass it around " + (startValue) + " bottles of beer on the wall.");
+
+for (startValue = 99; startValue >=2; startValue -=1) {
+  console.log((startValue) + " bottles of beer on the wall. Take one down. Pass it around " + (startValue) + " bottles of beer on the wall.");
+}
+if (startValue === 1) {
+   console.log((startValue) + " bottle of beer on the wall. Take it down and pass it around " + (startValue) + " bottle of beer on the wall.")
+}
