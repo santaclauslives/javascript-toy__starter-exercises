9db2906a4d5f5e75d4b27437aae8a87161d266db
Patrick Clark <patclark1958@gmail.com>
2016-03-11 15:22:51 -0600

    7h done last for loop

---
+++ 07_BasicLoops/global7h.js
@@ -0,0 +1,28 @@
+
+var startValue = parseInt(prompt("Please provide a number from which to start counting. No fractions and decimals please:  "));
+var endValue = parseInt(prompt("Please provide a number at which to stop counting. No fractions and decimals please: "));
+
+
+for (startValue; startValue <= endValue; startValue+=1) {
+
+  if ((startValue % 3 === 0) && (startValue % 5 === 0)) {
+    console.log("FizzBuzz");
+    //startValue+=1;
+  }
+
+  else if (startValue % 3 === 0) {
+    console.log("Fizz");
+    //startValue+=1;
+  }
+
+  else if (startValue % 5 === 0) {
+    console.log("Buzz");
+    //startValue+=1;
+  }
+
+  else {
+    console.log(startValue)
+   // startValue+=1;
+  }
+   
+}
